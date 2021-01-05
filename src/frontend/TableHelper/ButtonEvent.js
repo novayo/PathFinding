ec5d39f1791c6ca7b4a14acbd34f6618ca5f6e96
ChengTsungPao <abaozheng6@gmail.com>
2021-01-05 21:22:15 +0800

    status

--- src/frontend/TableHelper/ButtonEvent.js
+++ src/frontend/TableHelper/ButtonEvent.js
@@ -1,20 +1,26 @@
 import { useContext } from 'react'
 import { tableVar, touchContext, picture, setTable } from './TableIndex'
 import { SearchAnimation, PathAnimation } from './Animation'
+import { sysStatusContext, bombContext } from '../../Core';
 
 
 function ButtonEvent() {
     const touch = useContext(touchContext)
+    const sysStatus = useContext(sysStatusContext)
+    const bomb = useContext(bombContext)
 
     const Start = (search, path, speed) => {
+        sysStatus.set("RUNNING")
         console.log("Start")
         SearchAnimation(search, speed, 0)
-        setTimeout(() => {PathAnimation(path, speed, 0)}, speed * (search.length + 1))
+        setTimeout(() => {PathAnimation(path, speed, 0); sysStatus.set("IDLE")}, speed * (search.length + 1))
     }
 
     const Addbomb = () => {
+        bomb.set("True")
         console.log("AddBomb")
         setTable(Math.floor(tableVar.rowSize / 2) * tableVar.colSize + Math.floor(tableVar.colSize / 2), picture.bomb)
+        bomb.set("False")
     }
 
     const RemoveBomb = () => {
@@ -54,6 +60,7 @@ function ButtonEvent() {
         setTable(Math.floor(tableVar.rowSize / 2) * tableVar.colSize + Math.floor(tableVar.colSize / 4), picture.start)
         setTable(Math.floor(tableVar.rowSize / 2 + 1) * tableVar.colSize - Math.floor(tableVar.colSize / 4), picture.end)
         touch.set({componentKind: "", picture: ""})
+        bomb.set("False")
     }
 
     return { Start, Addbomb, RemoveBomb, ClearWalls, ClearPath, ClearBoard }
